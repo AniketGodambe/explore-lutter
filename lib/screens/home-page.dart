3b8b7dbfba25b7fe6204285391a412622a5360a2
Aniket Godambe <godambeaniket07@gmail.com>
2022-04-12 17:41:00 +0530

    json data add with file

--- lib/screens/home-page.dart
+++ lib/screens/home-page.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 import 'package:git_hubdemo/models/catalog.dart';
 import 'package:git_hubdemo/widgets.dart/drawer.dart';
 import 'package:git_hubdemo/widgets.dart/item-widget.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    var catalogJson = await rootBundle.loadString("assets/files/catalog.json");
+    var decodeData = jsonDecode(catalogJson);
+    var productData = decodeData["products"];
+    print(productData);
+  }
 
   @override
   Widget build(BuildContext context) {
